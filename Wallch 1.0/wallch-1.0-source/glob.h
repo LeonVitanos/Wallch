31d816d0376ea766d99170c37b5c0595508358ec
Leon Vitanos <leon.vitanos@gmail.com>
2023-04-10 15:35:15 +0200

    restructure

--- Wallch 1.0/wallch-1.0-source/glob.h
+++
@@ -1,7 +0,0 @@
-//the language int (for greek 2)
-extern int _language_;
-//the Start's int (not to have any problem with the Live Earth wallpaper (1 if running)
-extern int _start_running_;
-extern int _live_earth_running_;
-extern int minimize_to_tray;
-extern int pref;
