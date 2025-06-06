1420f8db3d80104a8c5a02a5003a579e0a71d4b6
Leon Vitanos <leon.vitanos@gmail.com>
2025-06-06 14:29:36 +0200

    fix(build): Restrict pragma silencing to Linux to fix cross-platform warnings

--- src/common/silenced_qfuturewatcher.h
+++ src/common/silenced_qfuturewatcher.h
@@ -6,7 +6,7 @@
 // Workaround for a C++20 style warning (-Wtemplate-id-cdtor) present in the constructor
 // of Qt's QFutureInterface<void> in Qt versions prior to 6.7.1.
 #if (QT_VERSION < QT_VERSION_CHECK(6, 7, 1))
-#if defined(__GNUC__) || defined(__clang__)
+#if defined(Q_OS_LINUX) && defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wtemplate-id-cdtor"
 #endif
@@ -15,7 +15,7 @@
 #include <QtCore/QFutureWatcher>
 
 #if (QT_VERSION < QT_VERSION_CHECK(6, 7, 1))
-#if defined(__GNUC__) || defined(__clang__)
+#if defined(Q_OS_LINUX) && defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic pop
 #endif
 #endif
