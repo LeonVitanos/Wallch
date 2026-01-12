dd0ad235def6bc4376fdecee01950eaba6debfa7
Leon Vitanos <leon.vitanos@gmail.com>
2026-01-12 21:38:01 +0100

    build(linux): silence compiler pragmas

--- src/common/silenced_qfuturewatcher.h
+++ src/common/silenced_qfuturewatcher.h
@@ -25,6 +25,7 @@
 #if (QT_VERSION < QT_VERSION_CHECK(6, 7, 1))
 #if defined(Q_OS_LINUX) && (defined(__GNUC__) || defined(__clang__))
 #pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpragmas"
 #if (defined(__GNUC__) && __GNUC__ >= 13) || (defined(__clang__) && __clang_major__ >= 18)
 #pragma GCC diagnostic ignored "-Wtemplate-id-cdtor"
 #endif
